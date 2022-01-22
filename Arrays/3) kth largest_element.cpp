08872ad4ebc5afd1778d4f86efa8a9e32b7271b1
SPARSHSHRI10 <sparshshrivastava10@gmail.com>
2022-01-22 18:55:33 +0530

    codes

---
+++ Arrays/3) kth largest_element.cpp
@@ -0,0 +1,59 @@
+(constant space method)
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
+{
+    //kth largest using binary search
+	int s = -1000000000;
+    int g = 1000000000;
+    int ans1;
+    while(s <= g)
+    {
+        int mid = s + (g-s)/2;
+        int cnt = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] >= mid)
+            {
+                cnt++;
+            }
+        }
+        if(cnt < k)
+        {
+            g = mid-1;
+        }
+        else
+        {
+            ans1 = mid;
+            s = mid+1;
+        }
+    }
+
+    vector<int>v;
+    v.push_back(ans1);
+    return v;
+}
+
+//using priority queue
+#include<bits/stdc++.h>
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
+{   
+    priority_queue<int,vector<int>,greater<int>>pq1;
+    for(int i=0;i<n;i++)
+    {
+        if(pq1.size() == k)
+        {
+            if(pq1.top() <= arr[i])
+            {
+                pq1.pop();
+                pq1.push(arr[i]);
+            }
+        }
+        else
+        {
+            pq1.push(arr[i]);
+        }
+    }
+    int ans1 = pq1.top();
+    vector<int>v;
+    v.push_back(ans1);
+    return v;
+}
---
+++ Arrays/3) kth smallest element.cpp
@@ -0,0 +1,62 @@
+(constant space method)
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
+{
+// kth smallest element using binary search
+	int s = -1000000000;
+    int g = 1000000000;
+    int ans;
+    while(s <= g)
+    {
+        int mid = s + (g-s)/2;
+        int cnt = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] <= mid)
+            {
+                cnt++;
+            }
+        }
+        if(cnt < k)
+        {
+            s = mid+1;
+        }
+        else
+        {
+            ans = mid;
+            g = mid-1;
+        }
+    }
+    
+    vector<int>v;
+    v.push_back(ans);
+    return v;
+}
+
+
+//using priority_queue
+#include<bits/stdc++.h>
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
+{
+    // kth smallest element
+	priority_queue<int>pq;
+    for(int i=0;i<n;i++)
+    {
+        if(pq.size() == k)
+        {
+            if(pq.top() >= arr[i])
+            {
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        else
+        {
+            pq.push(arr[i]);
+        }
+    }
+    int ans = pq.top();
+    
+    vector<int>v;
+    v.push_back(ans);
+    return v;
+}
